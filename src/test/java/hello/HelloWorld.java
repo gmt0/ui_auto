d4023c133c7a39f9febf5e1c9c5d13723ff01a02
M <m@mail.m>
2021-02-14 16:53:05 +0200

    added chrome

--- src/test/java/hello/HelloWorld.java
+++ src/test/java/hello/HelloWorld.java
@@ -3,6 +3,8 @@ package hello;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,10 +17,10 @@ public class HelloWorld {
 
     @Test
     public void helloWorld(){
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-        FirefoxOptions options = new FirefoxOptions();
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
-        WebDriver driver = new FirefoxDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, 15);
         driver.get("https://github.com/");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("header-search-input")));
